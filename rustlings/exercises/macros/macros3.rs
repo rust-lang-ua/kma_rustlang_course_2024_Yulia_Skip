ed91ef6f8872371eea85f23ff846c8555ec38454
Julia Skip <skipjulija01@gmail.com>
2024-10-18 19:56:21 +0300

    done threads, macros and quiz4

--- rustlings/exercises/macros/macros3.rs
+++ rustlings/exercises/macros/macros3.rs
@@ -2,7 +2,8 @@
 // Make me compile, without taking the macro out of the module!
 // Execute `rustlings hint macros3` for hints :)
 
-// I AM NOT DONE
+// I AM DONE
+#[macro_use]
 
 mod macros {
     macro_rules! my_macro {
